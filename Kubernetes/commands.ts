9e911e15cf2deada18cc4d030db0589d62b06c03
nomanali <aliyounas084@gmail.com>
2024-07-30 16:09:23 +0500

    main K8s and Kubectl command cove done

--- Kubernetes/commands.ts
+++ Kubernetes/commands.ts
@@ -1,3 +1,5 @@
+// Create Deployment in minikube
+
 // Main Commands 
 
 // kubectl get pods -> List all pods
@@ -13,3 +15,52 @@
 // kubectl get events -> List all events
 // kubectl get namespaces -> List all namespaces
 // kubectl get roles -> List all roles
+
+// To deploy a NGINX container in a pod run the following command
+
+// kubectl create deployment nginx-depl --image=nginx -> Create a deployment
+// nginx-depl -> Deployment name
+// --image=nginx -> Image to use from docker repository
+
+// layers of Abstraction's
+
+// - Deployment manages a ReplicaSet
+// - ReplicaSet manages Pods
+// - Pod manages Containers
+
+// Everything below the Deployment is managed by Kubernetes itself
+
+// kubectl edit deployment {nginx-depl} -> Edit a deployment
+// This will generate auto generated configuration file in the default editor with default editor
+
+// kubectl delete deployment {nginx-depl} -> Delete a deployment
+// This will delete the deployment and the replica set and the pods
+
+
+// Debugging the Pods
+
+// kubectl logs {pod-name} -> Get logs of a pod
+// kubectl describe pod {pod-name} -> Get detailed information about a pod
+// kubectl exec -it {pod-name} -- bin/bash -> Execute a command inside a pod to interact with the container
+
+
+// Delete Deployments / Apply configuration file
+
+// kubectl delete {container-name}  -> Delete a container
+
+// Configuration file
+// see -> nginx-deployment.yaml
+
+// kubectl apply -f {config-file} -> Apply a configuration file
+// Config file is a yaml file that contains the configuration of the deployment like the image, replicas, etc
+
+
+// K8s Yaml Configuration file in kubernetes
+
+// Each Configuration file has 2 main parts
+
+// - Metadata -> Data about the object like name, labels, etc
+// - Spec -> Specification of the object like image, replicas, etc
+// - Status -> Status of the object like the number of replicas, etc
+
+// see -> configuration-file-step.png for more details
