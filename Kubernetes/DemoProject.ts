3636d5fa6b8b8a0660665452dfcde453ae26e185
nomanali <aliyounas084@gmail.com>
2024-07-30 20:57:21 +0500

    DemoProject of mongoDB

---
+++ Kubernetes/DemoProject.ts
@@ -0,0 +1,9 @@
+// MongoDB and MongoDB-express
+
+// This is a simple project to demonstrate the use of Kubernetes.
+// see -> Overview of ProjectDemo.png
+
+
+// To create Secure base64 encoded password run the following command
+// echo -n 'username' | base64
+// echo -n 'password' | base64
